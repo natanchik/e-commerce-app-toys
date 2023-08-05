982ec1b75d3e0aa01fd907c2b099bdca7ca4e88a
Kristina Morozova <christina.vadi@gmail.com>
2023-08-05 08:43:57 +0600

    feat: add basic layout for main page

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,10 +1,13 @@
 import Header from './header';
+import MainPage from './mainPage';
 
 class App {
   startApp(): void {
     const header = new Header();
+    const mainPage = new MainPage();
 
     header.drawHeader();
+    mainPage.drawMainPage();
   }
 }
 
---
+++ src/components/mainPage.ts
@@ -0,0 +1,5 @@
+class MainPage {
+  drawMainPage(): void {}
+}
+
+export default MainPage;
