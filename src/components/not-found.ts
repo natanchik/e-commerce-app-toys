4e474e12e13f870a83dd90dd47ee7ce22a0a210b
Ахантьева Ирина Александровна <i.akhanteva@s7.ru>
2023-08-06 16:56:35 +0400

    feat: add not-found template and its styles

---
+++ src/components/not-found.ts
@@ -0,0 +1,28 @@
+import { createElement } from './utils';
+
+class NotFound {
+  public drawNotFound(): Node {
+    // const body = document.querySelector('body') as HTMLBodyElement;
+    const wrapper = createElement('div', ['not-found__wrapper']) as HTMLDivElement;
+    const info = createElement('div', ['not-found__info']) as HTMLDivElement;
+    const heading = createElement('h1', ['not-found__heading'], 'ERROR 404!') as HTMLElement;
+    const message = createElement(
+      'p',
+      ['not-found__message'],
+      'Oops! The page you are looking for could not be found.',
+    ) as HTMLParagraphElement;
+    const button = createElement(
+      'button',
+      ['not-found__button'],
+      'BACK TO HOME',
+    ) as HTMLButtonElement;
+    const img = createElement('div', ['not-found__img']) as HTMLDivElement;
+
+    info.append(heading, message, button);
+    wrapper.append(info, img);
+
+    return wrapper as Node;
+  }
+}
+
+export default NotFound;
