070fce1721fad21a0c7019f3062a6698d01d103b
Ахантьева Ирина Александровна <i.akhanteva@s7.ru>
2023-08-07 17:45:24 +0400

    refactor: replace categories in the file with constants

--- src/components/sidebar.ts
+++ src/components/sidebar.ts
@@ -1,25 +1,5 @@
 import { createElement } from './utils';
-
-const categories = {
-  'Shop by stage': [
-    'Baby Toys: 0-12M',
-    ' Toddler: 1-3 Years',
-    'Pre-School: 3-5 Years',
-    'School Age: 5 Years +',
-  ],
-  'Imaginative Play': [
-    'Play Kitchens & Shops',
-    'Role Play & Dress Up',
-    'Play Cars, Trains & Transport',
-  ],
-  'Educational Toys': [
-    'Montessori Toys',
-    'Shape Sorters',
-    'Games & Puzzles',
-    'Musical Instruments',
-  ],
-  'Outdoor Play': ['Sand & Water Play', 'Bikes, Trikes & Scooters', 'Garden Play & Discovery'],
-};
+import categories from './constants';
 
 class Sidebar {
   public drawSidebar(): Node {
