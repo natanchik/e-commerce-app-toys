0601b9c60e43887345352110be8d6d204aa3aebe
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-08-09 17:40:29 +0600

    refactor: add funcs for create elements

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -8,19 +8,88 @@ export const createElement = (tag: string, classes: string[], text?: string): HT
 
 export const createInputElement = (
   type: string,
-  text: string,
-  id: string,
-  parent: HTMLElement,
+  labelText: string,
+  inputId: string,
   page: string,
-): void => {
-  const inputBlock = createElement('div', [`${page}-item`]);
+  required: boolean = true,
+  attributes?: object,
+): HTMLDivElement => {
+  const label = createElement('label', [`${page}-label`], labelText);
+  label.setAttribute('for', inputId);
+
   const input = createElement('input', [`${page}-input`]) as HTMLInputElement;
   input.setAttribute('type', type);
-  input.setAttribute('id', id);
-  input.required = true;
+  input.setAttribute('id', inputId);
+  if (required) {
+    input.required = true;
+  }
+  if (attributes) {
+    Object.entries(attributes).forEach((entry) => input.setAttribute(entry[0], entry[1]));
+  }
 
-  const label = createElement('label', [`${page}-label`], text);
-  label.setAttribute('for', id);
+  const inputBlock = createElement('div', [`${page}-item`]) as HTMLDivElement;
   inputBlock.append(label, input);
-  parent.append(inputBlock);
+
+  return inputBlock;
+};
+
+export const createSelectElement = (
+  list: string[],
+  labelText: string,
+  inputId: string,
+  page: string,
+  required: boolean = true,
+): HTMLDivElement => {
+  const label = createElement('label', [`${page}-label`], labelText);
+  label.setAttribute('for', inputId);
+
+  const dataList = document.createElement('select');
+  dataList.classList.add(`${page}-input`);
+  dataList.setAttribute('id', inputId);
+  if (required) {
+    dataList.required = true;
+  }
+
+  for (let i = 0; i < list.length; i += 1) {
+    const option = document.createElement('option');
+    option.value = list[i];
+    option.textContent = list[i];
+    dataList.append(option);
+  }
+
+  const selectBlock = createElement('div', [`${page}-item`]) as HTMLDivElement;
+  selectBlock.append(label, dataList);
+
+  return selectBlock;
+};
+
+export const createCheckBoxElement = (
+  labelText: string,
+  inputId: string,
+  required: boolean = false,
+): HTMLDivElement => {
+  const label = createElement('label', ['checkbox-label'], labelText);
+  label.setAttribute('for', inputId);
+
+  const input = createElement('input', ['checkbox-input']) as HTMLInputElement;
+  input.setAttribute('type', 'checkbox');
+  input.setAttribute('id', inputId);
+  if (required) {
+    input.required = true;
+  }
+
+  const checkBoxBlock = createElement('div', ['checkbox-block']) as HTMLDivElement;
+  checkBoxBlock.append(input, label);
+
+  return checkBoxBlock;
+};
+
+export const showErrorMessage = (inputBlock: HTMLDivElement, message: string): void => {
+  const messageEl = createElement('p', ['error-message'], message);
+  inputBlock.append(messageEl);
+  const input = inputBlock.querySelector('.login-input');
+  input?.classList.add('error-input');
+  setTimeout(() => {
+    input?.classList.remove('error-input');
+  }, 3000);
 };
