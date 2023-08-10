62ac8a43de9a53dbb0dd9df5c8868b6d7c6a47b3
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-08-10 13:00:21 +0600

    feat: add same address for billing and shipping

--- src/components/autorization.ts
+++ src/components/autorization.ts
@@ -61,14 +61,34 @@ class LoginPage {
   }
 
   private drawAuthBlock(parent: HTMLElement): void {
-    parent.append(createInputElement('email', 'E-mail', 'email', 'login'));
-    parent.append(createInputElement('password', 'Password', 'password', 'login'));
+    parent.append(
+      createInputElement('email', 'E-mail', 'email', 'login', true, {
+        name: 'username',
+        autocomplete: 'username',
+      }),
+    );
+    parent.append(
+      createInputElement('password', 'Password', 'password', 'login', true, {
+        name: 'password',
+        autocomplete: 'current-password',
+      }),
+    );
   }
 
   private drawRegBlock(parent: HTMLElement): void {
     const emailBlock = createElement('div', ['login-row']);
-    emailBlock.append(createInputElement('email', 'E-mail*', 'email', 'login'));
-    emailBlock.append(createInputElement('password', 'Password*', 'password', 'login'));
+    emailBlock.append(
+      createInputElement('email', 'E-mail*', 'email', 'login', true, {
+        name: 'username',
+        autocomplete: 'username',
+      }),
+    );
+    emailBlock.append(
+      createInputElement('password', 'Password*', 'password', 'login', true, {
+        name: 'password',
+        autocomplete: 'new-password',
+      }),
+    );
     parent.append(emailBlock);
 
     const nameBlock = createElement('div', ['login-row']);
@@ -81,36 +101,46 @@ class LoginPage {
     userInfo.append(createSelectElement(salutation, 'Salutation', 'salutation', 'login', false));
     parent.append(userInfo);
 
-    parent.append(
-      createCheckBoxElement('Billing and shipping addresses are the same', 'are-same-addresses'),
+    const sameAddress = createCheckBoxElement(
+      'Billing and shipping addresses are the same',
+      'are-same-addresses',
     );
+    parent.append(sameAddress);
 
-    const addressTypes = ['billing', 'shipping'];
-    addressTypes.forEach((type) => {
-      const addressBlock = createElement('div', ['login-row', 'address-block']);
-      const addressTitle = createElement('p', ['address-title'], `Input your ${type} address`);
-
-      const userAddress = createElement('div', ['login-row']);
-      userAddress.append(createSelectElement(countries, 'Country*', 'country', 'login'));
-      userAddress.append(createInputElement('text', 'City*', 'city', 'login'));
-      userAddress.append(createInputElement('text', 'Street*', 'street', 'login'));
-      userAddress.append(
-        createInputElement('number', 'Postal code*', 'code', 'login', true, {
-          min: 10000,
-          max: 999999,
-        }),
-      );
-
-      const asDefault = createCheckBoxElement('Set as default address', `as-default-${type}`);
-
-      addressBlock.append(addressTitle, userAddress, asDefault);
-      parent.append(addressBlock);
-    });
+    const billingBlock = this.drawAddressBlock('billing');
+    const shippingBlock = this.drawAddressBlock('shipping');
+    parent.append(billingBlock, shippingBlock);
 
     const policyAgreeText =
       'I agree with <a href="">The terms of personal data processing</a> and <a href=""> Privacy policy</a>';
     parent.append(createCheckBoxElement(policyAgreeText, 'policyInput', true));
+
+    sameAddress.addEventListener('change', () => {
+      shippingBlock.classList.toggle('hidden');
+    });
   }
+
+  private drawAddressBlock = (type: string): HTMLDivElement => {
+    const addressBlock = createElement('div', ['login-row', 'address-block']) as HTMLDivElement;
+    addressBlock.setAttribute('id', `${type}-block`);
+    const addressTitle = createElement('p', ['address-title'], `Input your ${type} address`);
+
+    const userAddress = createElement('div', ['login-row']);
+    userAddress.append(createSelectElement(countries, 'Country*', `${type}-country`, 'login'));
+    userAddress.append(createInputElement('text', 'City*', `${type}-city`, 'login'));
+    userAddress.append(createInputElement('text', 'Street*', `${type}-street`, 'login'));
+    userAddress.append(
+      createInputElement('number', 'Postal code*', `${type}-code`, 'login', true, {
+        min: 10000,
+        max: 999999,
+      }),
+    );
+
+    const asDefault = createCheckBoxElement('Set as default address', `as-default-${type}`);
+    addressBlock.append(addressTitle, userAddress, asDefault);
+
+    return addressBlock;
+  };
 }
 
 export default LoginPage;
