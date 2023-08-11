dcf0478e463985ed41dda662de44902e5226ff83
Ахантьева Ирина Александровна <i.akhanteva@s7.ru>
2023-08-11 16:06:44 +0400

    refactor: update function for customer's creation

--- src/api/customer/createCustomer.ts
+++ src/api/customer/createCustomer.ts
@@ -1,3 +1,7 @@
+import getCustomerByID from './getCustomerByID';
+import { loginAfterRegistration } from './loginCustomer';
+
+// TODO remove this eslint-disable
 /* eslint-disable no-console */
 const myHeaders = {
   'Content-Type': 'application/json',
@@ -5,25 +9,89 @@ const myHeaders = {
 };
 
 // TODO take raw data from registration form
-const raw = JSON.stringify({
+const data = {
   email: 'TomWhite@example.com',
   firstName: 'Tom',
   lastName: 'White',
   password: 'secret1234',
   dateOfBirth: '1998-10-12',
+  salutation: 'Mr.',
   addresses: [
     { country: 'KZ', city: 'Shymkent', streetName: 'Gagarin', postalCode: '160000' },
     { country: 'KZ', city: 'Shymkent', streetName: 'Turkistanskaya', postalCode: '160000' },
   ],
-});
-
-const requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
 };
 
-const createCustomer = async (): Promise<void> => {
+// TODO add interfaces for addresses and acions
+const getCurrentActions = (
+  addresses: {
+    [key: string]: string;
+  }[],
+): { action: string; addressId: string }[] => {
+  const currentActions: { action: string; addressId: string }[] = [];
+  switch (addresses.length) {
+    case 1:
+      currentActions.push(
+        {
+          action: 'addBillingAddressId',
+          addressId: addresses[0].id,
+        },
+        {
+          action: 'addShippingAddressId',
+          addressId: addresses[0].id,
+        },
+      );
+      // TODO add checking of default value - if checkDefault or not
+      // if (checkDefault) {
+      //   currentActions.push(
+      //     {
+      //       action: 'setDefaultBillingAddress',
+      //       addressId: addresses[0].id,
+      //     },
+      //     {
+      //       action: 'setDefaultShippingAddress',
+      //       addressId: addresses[0].id,
+      //     },
+      //   );
+      // }
+      break;
+    case 2:
+      currentActions.push(
+        {
+          action: 'addBillingAddressId',
+          addressId: addresses[0].id,
+        },
+        {
+          action: 'addShippingAddressId',
+          addressId: addresses[1].id,
+        },
+      );
+      // TODO add checking of default value - if checkDefault or not
+      // if (checkDefaultBilling) {
+      //   currentActions.push({
+      //     action: 'setDefaultBillingAddress',
+      //     addressId: addresses[0].id,
+      //   });
+      // } else if (checkDefaultShipping) {
+      //   currentActions.push({
+      //     action: 'setDefaultShippingAddress',
+      //     addressId: addresses[1].id,
+      //   });
+      // }
+      break;
+  }
+
+  return currentActions;
+};
+
+const createCustomer = (): void => {
+  const raw = JSON.stringify(data);
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+  };
+
   fetch(
     'https://api.australia-southeast1.gcp.commercetools.com/ecommerce-application-jsfe2023/customers',
     requestOptions,
@@ -34,94 +102,44 @@ const createCustomer = async (): Promise<void> => {
         throw error;
       }
       if (res.status >= 200 && res.status < 300) {
-        return res;
+        return res.json();
       } else {
         switch (res.status) {
           case 400:
-            throw new Error(`This email address already exists please enter a unique one`);
+            throw new Error(
+              `This email address already exists, please log in or use another email address`,
+            );
           default:
-            throw new Error(`Catch error with status code ${res.status}`);
+            throw new Error(
+              `The error with status code ${res.status} has occured, please try later`,
+            );
         }
       }
     })
-    .then((res) => res?.json())
-    // TODO function() {if success --> login and redirect to main page}
-    .then(async (res) => {
+    // TODO function() {if success --> redirect to main page}
+    .then((res) => {
       const customerID = res.customer.id;
       const addresses = res.customer.addresses;
-      const currentActions: { action: string; addressId: string }[] = [];
-      switch (addresses.length) {
-        case 1:
-          currentActions.push(
-            {
-              action: 'addBillingAddressId',
-              addressId: addresses[0].id,
-            },
-            {
-              action: 'addShippingAddressId',
-              addressId: addresses[0].id,
-            },
-          );
-          // add checking of default value - if checkDefault or not
-          // if (checkDefault) {
-          //   currentActions.push(
-          //     {
-          //       action: 'setDefaultBillingAddress',
-          //       addressId: addresses[0].id,
-          //     },
-          //     {
-          //       action: 'setDefaultShippingAddress',
-          //       addressId: addresses[0].id,
-          //     },
-          //   );
-          // }
-          break;
-        case 2:
-          currentActions.push(
-            {
-              action: 'addBillingAddressId',
-              addressId: addresses[0].id,
-            },
-            {
-              action: 'addShippingAddressId',
-              addressId: addresses[1].id,
-            },
-          );
-          // add checking of default value - if checkDefault or not
-          // if (checkDefaultBilling && checkDefaultShipping) {
-          //   currentActions.push(
-          //     {
-          //       action: 'setDefaultBillingAddress',
-          //       addressId: addresses[0].id,
-          //     },
-          //     {
-          //       action: 'setDefaultShippingAddress',
-          //       addressId: addresses[1].id,
-          //     },
-          //   );
-          // } else if (checkDefaultBilling) {
-          //   currentActions.push({
-          //     action: 'setDefaultBillingAddress',
-          //     addressId: addresses[0].id,
-          //   });
-          // } else if (checkDefaultShipping) {
-          //   currentActions.push({
-          //     action: 'setDefaultShippingAddress',
-          //     addressId: addresses[1].id,
-          //   });
-          // }
-          break;
-      }
+      const currentActions = getCurrentActions(addresses);
       const dataForAddressActions = JSON.stringify({
         version: 1,
         actions: currentActions,
       });
-      await fetch(
+      fetch(
         `https://api.australia-southeast1.gcp.commercetools.com/ecommerce-application-jsfe2023/customers/${customerID}`,
         { method: 'POST', headers: myHeaders, body: dataForAddressActions },
       )
-        .then((response) => response.json())
-        .then((result) => console.log(result));
+        .then(() => {
+          const username = data.email;
+          const password = data.password;
+          loginAfterRegistration(username, password);
+          getCustomerByID(customerID);
+        })
+        .catch((err) => {
+          if (err instanceof Error) {
+            console.log(`${err.message}`);
+          }
+        });
     })
     .catch((err) => {
       if (err instanceof Error) {
