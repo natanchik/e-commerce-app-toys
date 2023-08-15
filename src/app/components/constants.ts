ab3552ea7362ef73557e6cd78c47dd99a0cc1959
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-08-15 16:30:29 +0600

    refactor: split validateInput func

--- src/app/components/constants.ts
+++ src/app/components/constants.ts
@@ -1,3 +1,5 @@
+import { ValidTemplates } from '../types/types';
+
 export const categories = {
   'Shop by stage': ['Baby Toys: 0-12M', ' Toddler: 1-3 Years', 'Pre-School: 3-5 Years', 'School Age: 5 Years +'],
   'Imaginative Play': ['Play Kitchens & Shops', 'Role Play & Dress Up', 'Play Cars, Trains & Transport'],
@@ -9,21 +11,21 @@ export const countries = { '': '', 'United States': 'US', Kazakhstan: 'KZ' };
 
 export const salutation = { '': '', Mr: 'Mr', Mrs: 'Mrs' };
 
-export const validTemplates = {
+export const validTemplates: ValidTemplates = {
   firstName: [/^[a-zA-Z]+$/, 'Must contain at least one character (use a-z, A-Z) and no special characters or numbers'],
   lastName: [/^[a-zA-Z]+$/, 'Must contain at least one character (use a-z, A-Z) and no special characters or numbers'],
   streetName: [/^[a-zA-Z0-9-]+$/, 'Must contain at least one character (use a-z, A-Z, 0-9, -)'],
   city: [/^[a-zA-Z-]+$/, 'Must contain at least one character (use a-z, A-Z, -) and no special characters or numbers'],
 };
 
-export const emailRequirements = [
+export const emailRequirements: [RegExp, string][] = [
   [/.{6,}/, 'Email address must be at least 6 characters long.'],
   [/@/, '<p>Email address must contain an "@" symbol separating local part and domain name.</p>'],
   [/^[^ ]*$/, '<p>Email address must not contain whitespace.</p>'],
   [/^[a-zA-Z0-9.@]*$/, 'Email address contains incorrect symbol (use a-z, A-Z, 0-9, .@).'],
 ];
 
-export const passwordRequirements = [
+export const passwordRequirements: [RegExp, string][] = [
   [/.{8,}/, 'Password must be at least 8 characters long.'],
   [/^[a-zA-Z0-9!@#$%^&*_+=/?.,~:;-]*$/, 'Password contains incorrect symbol (use a-z, A-Z, 0-9, !@#$%^&*_+=/?.,~:;-).'],
   [/[a-z]/, '<p>Password must contain at least one lowercase letter (use a-z).</p>'],
--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -7,3 +7,10 @@ export interface UrlInfo {
   pathname: string;
   cardId: string;
 }
+
+export interface ValidTemplates {
+  firstName: [RegExp, string];
+  lastName: [RegExp, string];
+  streetName: [RegExp, string];
+  city: [RegExp, string];
+}
