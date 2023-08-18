e5562ab31d844a75197cd6d7585c69a0dc0f9ea2
Kristina Morozova <christina.vadi@gmail.com>
2023-08-18 19:30:43 +0600

    fix: user login and logout methods

--- src/app/components/user.ts
+++ src/app/components/user.ts
@@ -1,4 +1,6 @@
 import getAccessToken from '../api/tokens/getAccessToken';
+import { UserState } from '../types/types';
+import { createElement, nullUserState } from './utils';
 
 class User {
   constructor() {
@@ -14,15 +16,25 @@ class User {
   }
 
   public userLogin(): void {
-    // const icons = document.querySelector('.header__icons') as HTMLDivElement;
-    // const beforeIcon = document.querySelector('.header__icon-bascket') as HTMLDivElement;
-    // const iconLogout = createElement('span', ['header__icon', 'header__icon-logout']) as HTMLSpanElement;
-    // const userName = createElement('span', ['header__name'], `${userState.firstName}`);
-    // icons.append(iconLogout);
-    //icons.insertBefore(userName, beforeIcon);
+    const userState: UserState = localStorage.getItem('userState')
+      ? JSON.parse(localStorage.getItem('userState') as string)
+      : nullUserState;
+    const icons = document.querySelector('.header__icons') as HTMLDivElement;
+    const beforeIcon = document.querySelector('.header__icon-bascket') as HTMLDivElement;
+    const logoutIcon = document.querySelector('header__icon-logout') as HTMLDivElement;
+    const userName = createElement('span', ['header__name'], `${userState.firstName}`);
+
+    logoutIcon.classList.remove('header__icon-logout_hidden');
+    icons.insertBefore(userName, beforeIcon);
   }
 
-  public userLogout(): void {}
+  public userLogout(): void {
+    const userName = document.querySelector('header__name') as HTMLDivElement;
+    const logoutIcon = document.querySelector('header__icon-logout') as HTMLDivElement;
+    logoutIcon.classList.add('header__icon-logout_hidden');
+    userName.remove();
+    localStorage.clear();
+  }
 
   private setEventListeners(): void {
     document.addEventListener('DOMContentLoaded', (): void => {
