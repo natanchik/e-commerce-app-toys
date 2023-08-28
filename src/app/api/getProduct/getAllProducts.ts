184b7f0007715fa99502574c0d03e71e0c69fd42
Kristina Morozova <christina.vadi@gmail.com>
2023-08-28 16:38:42 +0600

    fix: api get all products

--- src/app/api/getProduct/getAllProducts.ts
+++ src/app/api/getProduct/getAllProducts.ts
@@ -1,3 +1,5 @@
+import { Product } from '../../types/types';
+
 const getAllProducts = (): void => {
   const myHeaders = {
     'Content-Type': 'application/json',
@@ -9,12 +11,25 @@ const getAllProducts = (): void => {
     headers: myHeaders,
   };
 
+  const allProducts: Product[] = [];
+
   fetch(
-    'https://api.australia-southeast1.gcp.commercetools.com/ecommerce-application-jsfe2023/products',
+    'https://api.australia-southeast1.gcp.commercetools.com/ecommerce-application-jsfe2023/products?limit=500',
     requestOptions,
-  ).then((response) => response.text());
-  // .then((result) => console.log(result))
-  // .catch((error) => console.log('error', error));
+  )
+    .then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
+      } else {
+        throw new Error(`The error with status code ${response.status} has occured, please try later`);
+      }
+    })
+    .then((result) => {
+      result.results.forEach((product: Product) => {
+        allProducts.push(product);
+      });
+      localStorage.setItem('product_info', JSON.stringify(allProducts));
+    });
 };
 
 export default getAllProducts;
--- src/app/pages/catalog.ts
+++ src/app/pages/catalog.ts
@@ -1,5 +1,6 @@
 import getAllProducts from '../api/getProduct/getAllProducts';
 import { createElement } from '../components/utils';
+import { Price, Product } from '../types/types';
 
 class Catalog {
   constructor() {
@@ -11,9 +12,9 @@ class Catalog {
     const title = createElement('div', ['catalog__title'], '<h2>Catalog</h2>') as HTMLDivElement;
     const content = createElement('div', ['catalog__content']) as HTMLDivElement;
     const filters = this.drawFilters();
-    //const products = this.drawProducts();
+    const products = this.drawProducts();
 
-    //content.append(filters, products);
+    content.append(filters, products);
     catalog.append(title, content);
     return catalog;
   }
@@ -24,58 +25,58 @@ class Catalog {
     return filters;
   }
 
-  // private drawProducts(): HTMLDivElement {
-  //   const products = createElement('div', ['catalog__products']) as HTMLDivElement;
-  //   const allProducts: Product[] = localStorage.getItem('product_info')
-  //     ? JSON.parse(localStorage.getItem('product_info') as string)
-  //     : [];
+  private drawProducts(): HTMLDivElement {
+    const products = createElement('div', ['catalog__products']) as HTMLDivElement;
+    const allProducts: Product[] = localStorage.getItem('product_info')
+      ? JSON.parse(localStorage.getItem('product_info') as string)
+      : [];
 
-  //   allProducts.forEach((product: Product): void => {
-  //     const productBlock = this.drawProduct(product);
-  //     products.append(productBlock);
-  //   });
+    allProducts.forEach((product: Product): void => {
+      const productBlock = this.drawProduct(product);
+      products.append(productBlock);
+    });
 
-  //   return products;
-  // }
+    return products;
+  }
 
-  // private drawProduct(product: Product): HTMLDivElement {
-  //   console.log(product);
-  //   const productBlock = createElement('div', ['catalog__product', 'product']) as HTMLDivElement; 
-  //   const img = createElement('img', ['product__img']) as HTMLImageElement;
-  //   const url: string = product.masterData.current.masterVariant.images[0].url;
-  //   if (url) img.src = url;
-  //   const name = createElement('div', ['product__name'], `${product.masterData.current.name['en-US']}`) as HTMLDivElement;
-  //   const prices = this.drawPrices(product);
+  private drawProduct(product: Product): HTMLDivElement {
+    console.log(product);
+    const productBlock = createElement('div', ['catalog__product', 'product']) as HTMLDivElement; 
+    const img = createElement('img', ['product__img']) as HTMLImageElement;
+    const url: string = product.masterData.current.masterVariant.images[0].url;
+    if (url) img.src = url;
+    const name = createElement('div', ['product__name'], `${product.masterData.current.name['en-US']}`) as HTMLDivElement;
+    const prices = this.drawPrices(product);
 
-  //   productBlock.append(img, name, prices);
-  //   return productBlock;
-  // }
+    productBlock.append(img, name, prices);
+    return productBlock;
+  }
 
-  // private drawPrices(product: Product): HTMLDivElement {
-  //   const prices = createElement('div', ['product__prices']) as HTMLDivElement;
-  //   const priceValue = createElement('span', ['product__price']) as HTMLSpanElement;
-  //   const discountValue = createElement('span', ['product__discount']) as HTMLSpanElement;
-  //   let value: string;
-  //   let discount: string;
+  private drawPrices(product: Product): HTMLDivElement {
+    const prices = createElement('div', ['product__prices']) as HTMLDivElement;
+    const priceValue = createElement('span', ['product__price']) as HTMLSpanElement;
+    const discountValue = createElement('span', ['product__discount']) as HTMLSpanElement;
+    let value: string;
+    let discount: string;
 
-  //   product.masterData.current.masterVariant.prices.forEach((price: Price): void => {
-  //     if(price.country === 'US') {
-  //        value = (price.value.centAmount) ? (price.value.centAmount / (10 * price.value.fractionDigits)).toFixed(2) : '0.00';
-  //        priceValue.innerText = `${value} ${price.value.currencyCode}`;
+    product.masterData.current.masterVariant.prices.forEach((price: Price): void => {
+      if(price.country === 'US') {
+         value = (price.value.centAmount) ? (price.value.centAmount / (10 * price.value.fractionDigits)).toFixed(2) : '0.00';
+         priceValue.innerText = `${value} ${price.value.currencyCode}`;
 
-  //        if (price.discounted) {
-  //         discount = (price.discounted.value.centAmount / (10 * price.discounted.value.fractionDigits)).toFixed(2);
-  //         priceValue.classList.add('product__price_crossed-out');
-  //         discountValue.innerText = `${discount}`;
-  //         prices.append(discountValue);
-  //       }
-  //     }
-  //   })
+         if (price.discounted) {
+          discount = (price.discounted.value.centAmount / (10 * price.discounted.value.fractionDigits)).toFixed(2);
+          priceValue.classList.add('product__price_crossed-out');
+          discountValue.innerText = `${discount}`;
+          prices.append(discountValue);
+        }
+      }
+    })
     
-  //   prices.append(priceValue);
+    prices.append(priceValue);
 
-  //   return prices;
-  // }
+    return prices;
+  }
 }
 
 export default Catalog;
