38c650fe3224ab53b595d30a171ee1237d78d169
Ахантьева Ирина Александровна <i.akhanteva@s7.ru>
2023-09-04 15:56:27 +0400

    refactor: add tests for about, contacts and notfound pages

---
+++ src/tests/pages/about-us.test.ts
@@ -0,0 +1,21 @@
+import AboutUS from '../../app/pages/about-us';
+
+describe('Class AboutUS: drawAboutUs function', () => {
+  document.body.innerHTML = '';
+  const aboutUs = new AboutUS();
+  document.body.append(aboutUs.drawAboutUs());
+  const title = document.querySelector('.about-us__title');
+  const content = document.querySelector('.about-us__content')?.children;
+
+  it('should create about us page', () => {
+    expect(aboutUs).not.toBeNull();
+  });
+
+  it('should create title', () => {
+    expect(title?.innerHTML).toBe(`About us`);
+  });
+
+  it('should create content', () => {
+    expect(content?.length).toEqual(5);
+  });
+});
