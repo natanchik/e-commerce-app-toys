44f47ade58f1114d3286c0cce7ba2559f1eae5f6
Ахантьева Ирина Александровна <i.akhanteva@s7.ru>
2023-09-06 15:47:42 +0400

    feat: add functions to create cart and get all customer carts

---
+++ src/app/api/cart/createMyCart.ts
@@ -0,0 +1,26 @@
+export const createCart = async (): Promise<void> => {
+  const myHeaders = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${JSON.parse(localStorage.token_info).access_token}`,
+  };
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: JSON.stringify({
+      currency: 'USD',
+    }),
+  };
+
+  await fetch(
+    'https://api.australia-southeast1.gcp.commercetools.com/ecommerce-application-jsfe2023/me/carts',
+    requestOptions,
+  ).then((res) => {
+    if (res.status >= 200 && res.status < 300) {
+      return res.json();
+    } else {
+      throw new Error(`The error with status code ${res.status} has occured, please try later`);
+    }
+  });
+  // .then((res) => console.log(res));
+};
