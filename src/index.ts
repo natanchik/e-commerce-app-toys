b72f9a37e1403f70b02cb8bed6d2dd45408d1901
Kristina Morozova <christina.vadi@gmail.com>
2023-09-07 14:24:08 +0600

    feat: set interval for refresh token

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,15 @@ import getAnonymousToken from './app/api/tokens/getAnonymousToken';
 import getAllProducts from './app/api/getProduct/getAllProducts';
 import getCategories from './app/api/category/getCategories';
 import { hideLoading, showLoadig } from './app/components/utils';
+import refreshToken from './app/api/tokens/refreshToken';
 
 const loadTokens = async (): Promise<void> => {
   showLoadig();
   if (!localStorage.getItem('token_info') && localStorage.getItem('token_info') !== 'customer') await getAccessToken();
   if (!localStorage.getItem('anonymous_token')) await getAnonymousToken();
+  setInterval(() => {
+    refreshToken(JSON.parse(localStorage.anonymous_token).refreshToken);
+  }, 10800000);
   if (!localStorage.getItem('all_products')) await getAllProducts();
   if (!localStorage.getItem('categories')) await getCategories();
 };
