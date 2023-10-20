b4c052551208af4e32e978d8045295b41b92289b
Kristina Morozova <christina.vadi@gmail.com>
2023-10-20 12:30:13 +0600

    refactor: add new keys

--- src/app/api/helpers/constants.ts
+++ src/app/api/helpers/constants.ts
@@ -1,8 +1,8 @@
-export const URL = 'https://api.europe-west1.gcp.commercetools.com/';
+export const URL = 'https://auth.europe-west1.gcp.commercetools.com';
 
 export const PROJECT_KEY = 'ecommerce-rs2023';
 
-export const BASIC_URL: string = `${URL}${PROJECT_KEY}/`;
+export const BASIC_URL: string = `https://api.europe-west1.gcp.commercetools.com/${PROJECT_KEY}/`;
 
 export const AUTORIZATION_CODE: string =
-  'Basic dzBkZmVlLUhDalFqV1NuYlg2b0V6dktQOkhXNUtrN2JTdWhQWGhVVDJUQ3Nzb3p0WDJsbkhxMGRS';
+  'Basic MGlEYWN2R0V1YTRhR1BsX1kxcEprQWwtOkF3RnZrU2FtSUFoZ3g5WTNHMmJiSTRtM3FTQjlBeEt3';
